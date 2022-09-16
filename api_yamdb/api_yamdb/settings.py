e2925f244b67d7d8db20bcced0c91ee454915a32
Mikhail Golovin <76044731+MikhailGolovin1982@users.noreply.github.com>
2022-09-16 17:06:02 +0500

    Add action badge

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,10 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='12345')
 
 DEBUG = strtobool(os.getenv('DEBUG', default='False'))
 
-# ALLOWED_HOSTS = [
-#     os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split(',')
-# ]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = list(os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split(','))
 
 # Application definition
 INSTALLED_APPS = [
